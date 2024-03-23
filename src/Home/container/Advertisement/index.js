0b884a3bd23179008ad888ab424b4591a450b566
Winter0016 <chauquangphuc2604.2604@gmail.com>
2024-03-23 16:25:41 +0700

    improvised css

--- src/Home/container/Advertisement/index.js
+++ src/Home/container/Advertisement/index.js
@@ -3,7 +3,7 @@ import images from '../../../images/images'
 export const Advertisement = () => {
   return (
     <>
-        <section class=" bg-customYellow dark:bg-gray-900">
+        <section class="ad-custombg">
             <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 pt-5">
                 <div class="mr-auto place-self-center lg:col-span-7">
                     <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Chả Lụa<br/> Chà Bông<br/> Chất Lượng</h1>
