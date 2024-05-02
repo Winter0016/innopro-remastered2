c395be3a9c61c7c6d3e834bd7ed7f5cc69be5265
Winter0016 <chauquangphuc2604.2604@gmail.com>
2024-05-02 09:44:58 +0700

    dwfw

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -10,7 +10,8 @@ export const Home = () => {
   const myspeak = useRef(null);
 
 
-  const closecontact = () =>{
+  const closecontact = (e) =>{
+    e.preventDefault();
     // console.log(`myspeak : ${myspeak}`);
     myspeak.current.style.transition = "transform 1.5s ease";
     myspeak.current.style.transform = "translate(0,-100%)";
@@ -28,7 +29,7 @@ export const Home = () => {
             <>
                 <div className="speaktosale z-30" ref={myspeak} >
                   <div className="mx-auto w-full h-full max-w-screen-xl flex flex-col md:justify-center relative">
-                      <div id='x-speaktosale' onClick={() => closecontact()}>
+                      <div id='x-speaktosale' onClick={closecontact}>
                           <svg width="38" height="38" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 5L19 19M5 19L19 5" stroke="rgba(0,0,0,0.95)" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
                       </div>
                       <div className="flex flex-wrap gap-40 items-center">
